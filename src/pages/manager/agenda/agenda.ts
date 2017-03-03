b1a85f7a4e19e0578f9a0404ea28218ac32bd20d
Eraldo Energy <eraldo@eraldo.org>
2017-03-03 11:38:24 +0100

    Updated agenda view to show a simple date button.

--- src/pages/manager/agenda/agenda.ts
+++ src/pages/manager/agenda/agenda.ts
@@ -11,13 +11,11 @@ export class AgendaPage {
   scope: OutcomeScope;
   scopes: OutcomeScope[];
   date: Date;
-  isoDate: string;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
     this.scope = OutcomeScope.DAY;
     this.scopes = OUTCOME_SCOPES;
     this.date = moment().toDate();
-    this.isoDate = this.date.toISOString()
   }
 
   ionViewDidLoad() {
