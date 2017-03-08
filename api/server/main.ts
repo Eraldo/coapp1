d6cb2de9fc3ffc65b9a4947fc466c668f80df8a3
Eraldo Energy <eraldo@eraldo.org>
2017-03-08 21:24:07 +0100

    Added user fixture.

--- api/server/main.ts
+++ api/server/main.ts
@@ -1,12 +1,14 @@
-import { Meteor } from 'meteor/meteor';
+import {Meteor} from 'meteor/meteor';
 import {Outcomes} from "./collections/outcomes";
 import {Steps} from "./collections/steps";
 import * as moment from 'moment';
 import {OutcomeStatus, OutcomeScope} from "./models";
-import { ServiceConfiguration } from 'meteor/service-configuration';
+import {ServiceConfiguration} from 'meteor/service-configuration';
+import {Users} from "./collections/users";
 
 Meteor.startup(() => {
   // Meteor.absoluteUrl["defaultOptions"].rootUrl = "http://localhost:8100";
+  // TODO: Disable for production!
   loadfixtures();
   if (Meteor.settings) {
     configGoogle()
@@ -27,10 +29,66 @@ function configGoogle() {
 }
 
 function loadfixtures() {
+  loadUsers();
+  loadOutcomes();
+}
+
+function loadUsers() {
+  if (Users.find({}).cursor.count() === 0) {
+    let users = [];
+    let userId;
+
+    userId = Accounts.createUser({
+      email: 'tester1@mailinator.com',
+      password: 'tester1',
+      profile: {
+        name: 'Tester1'
+      }
+    });
+    Meteor.users.update(userId, {$set: {"emails.0.verified" :true}});
+    users.push(userId);
+
+    Accounts.createUser({
+      email: 'tester2@mailinator.com',
+      password: 'tester2',
+      profile: {
+        name: 'Tester2'
+      }
+    });
+    Meteor.users.update(userId, {$set: {"emails.0.verified" :true}});
+    users.push(userId);
+
+    Accounts.createUser({
+      email: 'tester3@mailinator.com',
+      password: 'tester3',
+      profile: {
+        name: 'Tester3'
+      }
+    });
+    Meteor.users.update(userId, {$set: {"emails.0.verified" :true}});
+    users.push(userId);
+
+    Accounts.createUser({
+      email: 'tester4@mailinator.com',
+      password: 'tester4',
+      profile: {
+        name: 'Tester4'
+      }
+    });
+    Meteor.users.update(userId, {$set: {"emails.0.verified" :true}});
+    users.push(userId);
+
+    return users;
+  }
+}
+
+function loadOutcomes() {
   if (Outcomes.find({}).cursor.count() === 0) {
+    let userId = Users.findOne()._id;
     let outcomeId;
 
     outcomeId = Outcomes.collection.insert({
+      userId: userId,
       name: 'clean garage',
       status: OutcomeStatus.OPEN,
       scope: OutcomeScope.DAY,
@@ -55,6 +113,7 @@ function loadfixtures() {
     });
 
     outcomeId = Outcomes.collection.insert({
+      userId: userId,
       name: 'colegend app',
       status: OutcomeStatus.OPEN,
       scope: OutcomeScope.DAY,
